af300760c2fe70b48f27b4d81d6731a3c4156fc3
Timinis <litimc3@gmail.com>
2018-08-09 14:58:59 -0700

    change 5 function to 1

--- js/app.js
+++ js/app.js
@@ -3,10 +3,10 @@
 // Get the user's name
 
 var userName = prompt('Please cite your name trespasser!');
-let correct = 0;
+var correct = 0;
 var totalQuestions = 0;
 alert(userName + '! Answer these questions correctly to proceed on! Answer with y/n or yes/no');
-
+var quiz;
 // if (correct answer) {
 // do this
 // and do this
@@ -20,80 +20,34 @@ alert(userName + '! Answer these questions correctly to proceed on! Answer with 
 //makes adding questions in the future much easier and the success rate adjust automatically
 //adjust the questions limit on the loop to add more questions and add question below by copying such format
 
-function questionOne() {
-  var answerOne = prompt('Are green tea and red tea from the same kind of tea leaves?').toLowerCase();
-  if (answerOne === 'yes' || answerOne === 'y') {
-    alert('That is correct! Here is your next question!');
-    correct++;
-  }
-  else {
-    alert('That is incorrect! This is not going well');
-  }
-  console.log(
-    'This question is designed for tea lovers ' + userName + ' answered ' +
-  answerOne
-  );
-  totalQuestions++;
-}
-questionOne();
+const startQuestion = (question, answer, flavor) => ({
+  question,
+  answer,
+  flavor
+});
+var questionList = [];
+questionList.push(startQuestion('Are green tea and red tea the same kind of leaves', 'yes', 'tea'));
+questionList.push(startQuestion('Are dogs believed to be the first species to be domesticated?', 'no', 'animal'));
+questionList.push(startQuestion('Are there an algorithm based on ants?', 'yes', 'algorithm'));
+questionList.push(startQuestion('Are endamame soy beans?', 'yes', 'sushi'));
+questionList.push(startQuestion('Are bubble teas healthy for you?', 'no', 'bubble tea'));
 
-function questionTwo() {
-  var answerTwo = prompt(
-    'Are dogs the first animal to be domesticated by humans?'
-  ).toLowerCase();
-  if (answerTwo === 'no' || answerTwo === 'n') {
+console.log(questionList[0]);
+const answerQuestion = (quizQuestions) => {
+  var response = prompt(quizQuestions.question).toLowerCase();
+  if (response.charAt(0) === quizQuestions.answer.charAt(0)) {
     alert('That is correct! Here is your next question!');
     correct++;
   } else {
-    alert('That is incorrect! This is not going well.');
+    alert('That is incorrect!');
   }
-  console.log(
-    'This question is designed for animal lovers ' + userName + ' answered ' + answerTwo
-  );
+  console.log('This question is designed for ' + quizQuestions.flavor + ' lovers');
   totalQuestions++;
-}
-questionTwo();
+};
 
-function questionThree() {
-  var answerThree = prompt('Are edamame soybeans?').toLowerCase();
-  if (answerThree === 'y' || answerThree === 'yes') {
-    alert('That is correct! Here is your next question!');
-    correct++;
-  } else {
-    alert('That is incorrect! This is not going well.');
-  }
-  console.log(
-    'This question is designed for sushi lovers ' + userName + ' answered ' + answerThree
-  );
-  totalQuestions++;
+for (quiz = 0; quiz < questionList.length; quiz++) {
+  answerQuestion(questionList[quiz]);
 }
-questionThree();
-
-function questionFour() {
-  var answerFour = prompt('Are there an algorithm based on ants?').toLowerCase();
-  if (answerFour === 'y' || answerFour === 'yes') {
-    alert('That is correct! Here is your next question!');
-    correct++;
-  } else {
-    alert('That is incorrect! This is not going well.');
-  }
-  console.log('This question is designed for algorithm lovers ' + userName + ' answered ' + answerFour);
-  totalQuestions++;
-}
-questionFour();
-
-function questionFive() {
-  var answerFive = prompt('Are bubble teas healthy?').toLowerCase();
-  if (answerFive === 'n' || answerFive === 'no') {
-    alert('That is correct, bubble tea is high in sugar!!!');
-    correct++;
-  } else {
-    alert('That is incorrect, tapiocas are a form of starch!!');
-  }
-  console.log('This question is designed for tapioca lovers ' + userName + ' answered ' + answerFive);
-  totalQuestions++;
-}
-questionFive();
 
 function questionSix() {
   var guessSix = 0;
